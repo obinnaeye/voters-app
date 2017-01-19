3bf332305e28ddaf9c7071ae2b4dfdc1ae1d8465
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-01-19 19:26:49 +0000

    add polls and votes route to index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,66 @@
-var express = require("express");
-var app= express();
+'use strict';
 
-app.get("/", function(req, res){
-    res.sendFile( '/home/ubuntu/workspace/voting-app/public/index.html');
-});
+var path = process.cwd();
 
-app.listen(process.env.PORT, function(){
-    console.log("app listening on port: ", process.env.PORT)
-})
+var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+
+
+
+module.exports = function (app, passport) {
+    function isLoggedIn (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        } else {
+            res.redirect('/login');
+        }
+    }
+    
+    var clickHandler = new ClickHandler();
+
+    app.route('/')
+        .get(isLoggedIn, function (req, res) {
+            res.sendFile(path + '/public/index.html');
+    });
+    
+    app.route('/login')
+    .get(function (req, res) {
+        res.sendFile(path + '/public/login.html');
+    });
+    
+    app.route('/logout')
+    .get(function (req, res) {
+        req.logout();
+        res.redirect('/login');
+    });
+    
+    app.route('/profile')
+    .get(isLoggedIn, function (req, res) {
+        res.sendFile(path + '/public/profile.html');
+    });
+    
+    app.route('/api/:id')
+    .get(isLoggedIn, function (req, res) {
+        res.json(req.user.github);
+    });
+    
+    app.route('/auth/github')
+    .get(passport.authenticate('github'));
+    
+    app.route('/auth/github/callback')
+    .get(passport.authenticate('github', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+    //create created polls for authenticated individuals
+    //add new polls
+    //delete polls by user
+    app.route('/api/:id/polls')
+    .get(isLoggedIn, clickHandler.getClicks)
+    .post(isLoggedIn, clickHandler.addClick)
+    .delete(isLoggedIn, clickHandler.resetClicks);
+    
+    app.route('/api/:id/votes')
+    .get(isLoggedIn, clickHandler.getClicks)
+    .post(isLoggedIn, clickHandler.addClick)
+    .delete(isLoggedIn, clickHandler.resetClicks);
+};
